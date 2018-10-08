a281eb95b5c5643e0b2af0ead0baf708129a5edc
StrikeEagleCC <35362359+StrikeEagleCC@users.noreply.github.com>
2018-10-08 00:57:55 -0500

    Add interrupts, and other functions
    Split the odrive reset and calibration stuff into it's own function resetOdrive().
    
    Split the PS3 controller connection stuff into its own function controllerConnected().
    
    Implemented interrupts for target detection.
    
    changed servo setPosition playtime parameters to 255, as they were being truncated anyway.
    
    Made skeleton framework for getting wheel positions from ODrive and sending them to ROS with the ROScomm() function.
    
    Added automode overrides to driveCtl() by passing it auto theta and velocity

--- arduino_mega/arduino_mega.ino
+++ arduino_mega/arduino_mega.ino
@@ -44,13 +44,15 @@ XYZrobotServo gripper(servo_serial, 4);
 #define ROS_serial Serial1
 
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ PINOUT ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-int const ODriveReset = 52;
-int const battLED = 6;
-int const BTconnectLED = 13;
-int const shutdownPin = 8;
-int const detectorEnablePin = 999;
-int const DetectorTriggerPin = 999;
-int const detectorAlertPin = 999;
+const byte ODriveReset = 52;
+const byte battLED = 50;
+const byte BTconnectLED = 51;
+const byte autoModeLED = 49;
+const byte shutdownPin = 8;
+
+//interrupts
+const byte ltTargetDetect = 20;
+const byte rtTargetDetect = 21;
 
 /*~~~~~~~~~~~~~~~~~~~~~~ CONTROLLER SETTINGS ~~~~~~~~~~~~~~~~~~~~~~~~*/
 int ltAnalogXDeadZone = 10;  //set deazone ranges each axis on each stick. Values will probably be less than 10
@@ -69,7 +71,7 @@ float analogR2Scaler = 1;
 
 /*~~~~~~~~~~~~~~~~~~~~~~~~ DRIVING SETTINGS ~~~~~~~~~~~~~~~~~~~~~~~~~*/
 int steeringTrim      = 0.0;   // In radians, negative for left bias, positive for right bias
-int accelerationTime  = 1000;  //milliseconds to transition from full reverse to full forward. Higher value means slower acceleration (and deceleration)
+unsigned int accelerationTime  = 1000;  //milliseconds to transition from full reverse to full forward. Higher value means slower acceleration (and deceleration)
 
 
 /*~~~~~~~~~~~~~~~~~~~~~~~~~ SERVO SETTINGS ~~~~~~~~~~~~~~~~~~~~~~~~~~*/
@@ -94,7 +96,7 @@ long armMidAngleLimit[2]   = {-165000, 165000};
 long wristAngleLimit[2]    = {-165000, 165000};
 long gripperAngleLimit[2]  = {-165000, 165000};
 
-int servoUpdateSpeed  = 2; //in 10's of milliseconds (2 = 20ms)
+unsigned int servoUpdateSpeed  = 2; //in 10's of milliseconds (2 = 20ms)
 
 
 /*~~~~~~~~~~~~~~~~~~~~~~~~ BATTERY SETTINGS ~~~~~~~~~~~~~~~~~~~~~~~~~*/
@@ -146,15 +148,18 @@ int armMidPos      = 0;
 int wristPos       = 0;
 int gripperPos     = 0;
 
-unsigned long currentMillis = 0;
+//autoMode drive command variables
+float steeringThetaAuto = 0;
+float driveRAuto        = 0;
 
+long motorPos[2] = {0, 0};  // motor positions, lt and rt respectively
+
+unsigned long currentMillis  = 0;
+volatile byte targetDetected = 0;  // 0 = no target, 1 = left side, 2 = right side
 
 void setup() {
-  Serial.begin(115200);
-  #if !defined(__MIPSEL__) 
-    while(!Serial); //Wait for serial port to connect - used on Leonardo, Teensy, and otehr boards with built in USB CDC serial connection
-    //^^^is this necessary for arduino then?
-  #endif
+  Serial.begin(115200);  
+  
   if (Usb.Init() == -1) {  //presumably, this if statement stops the boot if something doesnt start.
     Serial.print(F("\r\nOSC did not start"));  //what is the "F" in the argument here? and what is \nOSC?
     while (1); //halt  //is the purpose of this just to hang the board if the usb device isn't initialized?
@@ -167,66 +172,26 @@ void setup() {
   ROS_serial.begin(115200);
 
   // Set pinmodes
+  pinMode(ltTargetDetect, INPUT);
+  pinMode(rtTargetDetect, INPUT);
   pinMode(ODriveReset, OUTPUT);
   pinMode(battLED, OUTPUT);
   pinMode(BTconnectLED, OUTPUT);
   pinMode(shutdownPin, OUTPUT);
   digitalWrite(shutdownPin, LOW);
 
+  // Configure interrupts
+  attachInterrupt(digitalPinToInterrupt(ltTargetDetect), ltTarget, RISING);
+  attachInterrupt(digitalPinToInterrupt(rtTargetDetect), rtTarget, RISING);
+
   // Reset the ODrive
-  Serial.println("Resetting ODrive . . .");
-  digitalWrite(ODriveReset, LOW);
-  delay(100);
-  digitalWrite(ODriveReset, HIGH);
-  delay(100);
+  resetOdrive();
 
-  //wait for ODrive to come online
-  odrive_serial << "r vbus_voltage\n";
-  while(odrive.readFloat() == 0.00) {
-    delay(100);
-    odrive_serial << "r vbus_voltage\n";
+  //connect PS3 controller
+  while (!controllerConnected()) {
+    delay(10);
   }
-
-  // Once the ODrive is online, battCheck will work
-  battCheck();
-
-  //run calibrations
-  Serial.println("Calibrating motors . . .");
-  int requested_state = ODriveArduino::AXIS_STATE_MOTOR_CALIBRATION;
-  odrive.run_state(0, requested_state, false);
-  odrive.run_state(1, requested_state, true);
-
-  Serial.println("Calibrating Encoders . . .");
-  requested_state = ODriveArduino::AXIS_STATE_ENCODER_OFFSET_CALIBRATION;
-  odrive.run_state(0, requested_state, false);
-  odrive.run_state(1, requested_state, true);
-
-  Serial.println("Setting velocity control mode . . .");
-  odrive_serial << "w axis0.controller.config.control_mode 2\n";
-  odrive_serial << "w axis1.controller.config.control_mode 2\n";
-  delay(10);
-
-  Serial.println("Setting closed loop control . . .");
-  requested_state = ODriveArduino::AXIS_STATE_CLOSED_LOOP_CONTROL;
-  odrive.run_state(0, requested_state, false);
-  odrive.run_state(1, requested_state, false);
   
-  //check both motors are in closed loop control mode
-  int timeout_ctr = 100;
-  int stateCheck = 0;
-  do {
-    delay(100);
-    odrive_serial << "r axis0.current_state\n";
-    if(odrive.readInt() != requested_state) stateCheck = ++stateCheck;
-    odrive_serial << "r axis1.current_state\n";
-    if(odrive.readInt() != requested_state) stateCheck = ++stateCheck;
-    if(timeout_ctr = 1) Serial.println ("Could not set closed loop control");
-  } while (stateCheck != 0 || --timeout_ctr > 0);
-  
-  if(timeout_ctr > 0) Serial.println("Motors are good to go!");
-
-  controllerConnect();
-
   // Get and set servo positions/angles
   armBasePos = armBase.readPosition();
   armMidPos =  armMid.readPosition();
@@ -239,31 +204,39 @@ void setup() {
   gripperAngle = map(gripperPos, 0, 1023, -165000, 165000);
 }
 
-  
 void loop() {
   battCheck();
   getCtlInputs();
-  inputCtlMod();
-
-  //monitor for shutdown signal
+  
+  // Monitor for shutdown signal
   if (dPadUp) {
     digitalWrite(shutdownPin, HIGH);
-    Serial.print("Emergency shutdown signal recieved.");
+    Serial.print(F("Emergency shutdown signal recieved."));
     while(1); //halt
   }
-
-  if (start == true && armHome) {  //toggle auto mode
+  
+  inputCtlMod();
+  
+  // Toggle auto mode
+  if (start && armHome) {
     autoMode = !autoMode;
+    noInterrupts();        //disable interrupts while modifying a volitile variable
+    targetDetected = 0;
+    interrupts();
+    if (autoMode) {
+      //tell ROS we're in autoMode
+    } else {
+      //tellROS we're no longer in autoMode
+    }
   }
 
-  //enable control of arm if not in auto mode
-  if (!autoMode) {
-    armCtl;
-  }
+  // Enable control of arm only if not in auto mode
+  if (!autoMode) armCtl();
+
+  ROScomm();
   
   driveCtl();
 }
-
 
 void driveCtl() {
 /*
@@ -275,11 +248,19 @@ void driveCtl() {
  * as forward velocity increases.
  */
  
-//Convert to polar coordinates  
-  float steeringTheta = atan2(ltAnalogX, ltAnalogY); //determine angle of velocity vector
-  steeringTheta = steeringTheta + steeringTrim; //apply steering trim
-  float driveR = sqrt(square(ltAnalogX)+square(ltAnalogY)); //determine the magnitude of the velocity vector
-  driveR = constrain(driveR, 0.0, 128.0);
+//Convert lt analog stick X and Y coordinates to polar coordinates
+  float steeringTheta;
+  float driveR;
+  if (!autoMode) { 
+    steeringTheta = atan2(ltAnalogX, ltAnalogY); //determine angle of velocity vector
+    steeringTheta = steeringTheta + steeringTrim; //apply steering trim
+    driveR = sqrt(square(ltAnalogX)+square(ltAnalogY)); //determine the magnitude of the velocity vector
+    driveR = constrain(driveR, 0.0, 128.0);
+  }else {
+    steeringTheta = steeringThetaAuto;
+    driveR = driveRAuto;
+  }
+  
   unsigned int driveRscaled = driveR * 511; //scale up for application of acceleration limits. effectively constrained to 65,408
       
   //Serial << "Steering Angle: \t" << (steeringTheta * 57.2957) << "\tVelocity Magnituded: \t" << driveR << "\n\n";
@@ -406,39 +387,44 @@ void armCtl() {
   }
 }
 
-
-void autoModeCtl() {
-  
-}
-
-void getCtlInputs () {
+void getCtlInputs() {
 /*This function gets all the control inputs from the PS3 controller. It would probably
  * be faster to only request each input as it is needed, but this is clean and simple.
  * In the meantime, comment out any inputs that arent needed by the robot.
  */
   Usb.Task();
-  if (PS3.PS3Connected) {
-      ltAnalogX = PS3.getAnalogHat(LeftHatX);  // get right stick X and Y positions and center on zero
-      ltAnalogY = PS3.getAnalogHat(LeftHatY);
-  //    rtAnalogX = PS3.getAnalogHat(RightHatX);
-      rtAnalogY = PS3.getAnalogHat(RightHatY);
-      l1 = PS3.getButtonPress(L1);
-      r1 = PS3.getButtonPress(R1);
-      analogL2btn = PS3.getAnalogButton(L2);
-      analogR2btn = PS3. getAnalogButton(R2);
-  //    l3 = PS3.getButtonClick(L3);
-  //    r3 = PS3.getButtonClick(R3);
-      tri = PS3.getButtonClick(TRIANGLE);
-      circ = PS3.getButtonClick(CIRCLE);
-      cross = PS3.getButtonClick(CROSS);
-      sqr = PS3.getButtonClick(SQUARE);
-      dPadUp = PS3.getButtonPress(UP);
-      dPadDown = PS3.getButtonPress(DOWN);
-  //    dPadLeft = PS3.getButtonClick(LEFT);
-  //    dPadRight = PS3.getButtonClick(RIGHT);
-      start = PS3.getButtonClick(START);
-      select = PS3.getButtonClick(SELECT);
-      psBtn = PS3.getButtonClick(PS);
+  if (controllerConnected()) {
+    ltAnalogX = PS3.getAnalogHat(LeftHatX);  // get right stick X and Y positions and center on zero
+    ltAnalogY = PS3.getAnalogHat(LeftHatY);
+//    rtAnalogX = PS3.getAnalogHat(RightHatX);
+    rtAnalogY = PS3.getAnalogHat(RightHatY);
+    l1 = PS3.getButtonPress(L1);
+    r1 = PS3.getButtonPress(R1);
+    analogL2btn = PS3.getAnalogButton(L2);
+    analogR2btn = PS3. getAnalogButton(R2);
+//    l3 = PS3.getButtonClick(L3);
+//    r3 = PS3.getButtonClick(R3);
+    tri = PS3.getButtonClick(TRIANGLE);
+    circ = PS3.getButtonClick(CIRCLE);
+    cross = PS3.getButtonClick(CROSS);
+    sqr = PS3.getButtonClick(SQUARE);
+    dPadUp = PS3.getButtonPress(UP);
+    dPadDown = PS3.getButtonPress(DOWN);
+//    dPadLeft = PS3.getButtonClick(LEFT);
+//    dPadRight = PS3.getButtonClick(RIGHT);
+    start = PS3.getButtonClick(START);
+    select = PS3.getButtonClick(SELECT);
+    psBtn = PS3.getButtonClick(PS);
+
+  //turn on controller LEDs for found target
+    if (autoMode) {
+      if (targetDetected == 1) PS3.setLedOn(LED3);
+      if (targetDetected == 2) PS3.setLedOn(LED4);
+    }else {
+      PS3.setLedOff(LED3);
+      PS3.setLedOff(LED4);
+    }
+    
     digitalWrite(BTconnectLED, HIGH);
   }else{
     ltAnalogX = 127;  //reset buttons and axes if the controller drops out
@@ -462,11 +448,8 @@ void getCtlInputs () {
     start = false;
     select = false;
     psBtn = false;
-    
-    controllerConnect();
   }
 }
-
 
 void inputCtlMod () {
   /*This function centers analog stick inputs on zero, flips the necessary axes,
@@ -503,7 +486,6 @@ void inputCtlMod () {
 //  Serial << "Axis values after deadzone: X: " << ltAnalogX << "\tY: " << ltAnalogY << '\n';
 }
 
-
 void deactivateArm() {
 /*This function sends the arm to the home (collapsed) position, and sets servo speeds to zero
  */
@@ -520,8 +502,8 @@ void deactivateArm() {
     gripperPos = map(gripperHomeAngle, -165000, 165000, 0, 1023);
 
     //send servos to home position
-    armBase.setPosition(armBasePos, 500);
-    armMid.setPosition(armMidPos, 500);
+    armBase.setPosition(armBasePos, 255);
+    armMid.setPosition(armMidPos, 255);
     wrist.setPosition(wristPos,200);
     gripper.setPosition(gripperPos, 100);
   }
@@ -569,13 +551,13 @@ static unsigned long activateMillis = 0;
     gripperPos = map(gripperReadyAngle, -165000, 165000, 0, 1023);
 
     //send servos to ready position
-    armBase.setPosition(armBasePos, 500);
-    armMid.setPosition(armMidPos, 500);
+    armBase.setPosition(armBasePos, 255);
+    armMid.setPosition(armMidPos, 255);
     wrist.setPosition(wristPos,200);
     gripper.setPosition(gripperPos, 100);
   }
   
-  if (millis - activateMillis > 1000) { //run the following check once per second
+  if (millis() - activateMillis > 1000) { //run the following check once per second
     //check if servos are at ready position
     boolean servosReady[4] = {false, false, false, false};
     int servoPos[4];
@@ -595,6 +577,17 @@ static unsigned long activateMillis = 0;
   activateMillis = millis();
 }
 
+void ROScomm() {
+  // Get motor/encoder positions
+  for (int i = 0; i < 2; i++) {
+    odrive_serial << "w axis" << i << ".encoder.count_in_cpr\n";
+    delay(10);
+    motorPos[i] = odrive.readInt();
+  }
+  // Pass to ROS
+  
+  // Get steering angle and velocity
+}
 void battCheck(){
 /* This function samples the battery voltage at intervals
  *  and averages a number of samples before assigning 
@@ -712,27 +705,93 @@ void battCheck(){
 
 boolean odriveCheck(){
   //use this function to check the axis, motors, and encoders for errors, as well as for the correct control modes
+  return true;
 }
 
-void controllerConnect() {
+boolean controllerConnected() {
   // Wait for PS3 controller to connect
+  static int BTconnectLEDState = LOW;
   if (!PS3.PS3Connected){
-    Serial.println("Waiting for PS3 controller...");
+    Serial.println(F("PS3 controller disconnected . . ."));
     unsigned long connectPrevMillis = 0;
-    static int BTconnectLEDState = HIGH;
-    digitalWrite(BTconnectLED, HIGH);
-    while (!PS3.PS3Connected){
-      battCheck();
-      Usb.Task();
-      if (millis() - connectPrevMillis > 250) {
-        connectPrevMillis = millis();
-        BTconnectLEDState = !BTconnectLEDState;
-        digitalWrite(BTconnectLED, BTconnectLEDState);
-      }
-      delay(10);
-    }
-    Serial.println("Controller connected");
+    battCheck();
+    Usb.Task();
+    if (millis() - connectPrevMillis > 250) {
+      connectPrevMillis = millis();
+      BTconnectLEDState = !BTconnectLEDState;
+      digitalWrite(BTconnectLED, BTconnectLEDState);
+    } 
+    return false;
+  }else {
+    Serial.println(F("Controller connected"));
     BTconnectLEDState = HIGH;
-    digitalWrite(BTconnectLED, BTconnectLEDState); 
-  }    
+    digitalWrite(BTconnectLED, BTconnectLEDState);
+    return true;
+  }
 }
+
+void resetOdrive() {
+  Serial.println("Resetting ODrive . . .");
+  digitalWrite(ODriveReset, LOW);
+  delay(100);
+  digitalWrite(ODriveReset, HIGH);
+  delay(100);
+
+  //wait for ODrive to come online
+  odrive_serial << "r vbus_voltage\n";
+  while(odrive.readFloat() == 0.00) {
+    delay(100);
+    odrive_serial << "r vbus_voltage\n";
+  }
+
+  // Once the ODrive is online, battCheck will work
+  battCheck();
+
+  //run calibrations
+  Serial.println("Calibrating motors . . .");
+  int requested_state = ODriveArduino::AXIS_STATE_MOTOR_CALIBRATION;
+  odrive.run_state(0, requested_state, false);
+  odrive.run_state(1, requested_state, true);
+
+  Serial.println("Calibrating Encoders . . .");
+  requested_state = ODriveArduino::AXIS_STATE_ENCODER_OFFSET_CALIBRATION;
+  odrive.run_state(0, requested_state, false);
+  odrive.run_state(1, requested_state, true);
+
+  Serial.println("Setting velocity control mode . . .");
+  odrive_serial << "w axis0.controller.config.control_mode 2\n";
+  odrive_serial << "w axis1.controller.config.control_mode 2\n";
+  delay(10);
+
+  Serial.println("Setting closed loop control . . .");
+  requested_state = ODriveArduino::AXIS_STATE_CLOSED_LOOP_CONTROL;
+  odrive.run_state(0, requested_state, false);
+  odrive.run_state(1, requested_state, false);
+  
+  //check both motors are in closed loop control mode
+  int timeout_ctr = 100;
+  boolean stateCheck[2] = {false, false};
+  do {
+    delay(100);
+    for (byte i = 0; i < 2; i++){
+      odrive_serial << "r axis" << i <<".current_state\n";
+      delay(10);
+      if(odrive.readInt() == requested_state) stateCheck[i] = true; 
+    }
+    if (stateCheck[1] && stateCheck[2]) {
+      Serial.println("Motors are good to go!");
+      return;
+    }
+  } while (timeout_ctr-- > 0);
+  
+  Serial.println ("Could not set closed loop control");
+
+}
+/*~~~~~~~~~~~~~~~~ INTERRUPT SERVICE ROUTINES (ISR) ~~~~~~~~~~~~~~~~~*/
+void ltTarget() {
+  if (autoMode) targetDetected = 1;
+}
+void rtTarget() {
+  if (autoMode) targetDetected = 2;
+}
+// End of ISRs
