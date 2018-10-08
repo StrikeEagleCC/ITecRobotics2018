84cd65f85becad2d37755b840e58f35b2472ee01
StrikeEagleCC <35362359+StrikeEagleCC@users.noreply.github.com>
2018-10-07 20:45:41 -0500

    Overhaul arm control, IR_sensor.ino
    arduino_mego.ino:
    
    overhauled armctrl(), activateArm() and deactivateArm() functions. Deleted moveServos() function.
    
    created controllerConnect() function and called it in setup() and getControlInputs() instead of duplicating that code in two places.
    
    Overhauled battCheck() function to be more configurable and to use arrays instead of giant switch case statements and ten variables.
    
    Minor formatting changes.
    
    IR_sensor.ino:
    overhauled entire thing. Added left and right output pins.  Switch to 2 dimensional array to facilitate clean averaging. Integrated contrastCheck() functionality back into loop().

--- IR_sensor/IR_sensor.ino
+++ IR_sensor/IR_sensor.ino
@@ -1,69 +1,161 @@
 #include <QTRSensors.h>
 
-/* These two lines make it simpler to concatenante serial prints. For example, istead of:
- *  Serial.print("Voltage: ");
- *  Serial.print(vbus_voltage);
- *  Serial.println("V");
- *  
- *  you can instead use:
- *  Serial << "Voltage: " << vbus_voltage << "V\n";
- */
-template<class T> inline Print& operator <<(Print &obj,     T arg) { obj.print(arg);    return obj; }
-template<>        inline Print& operator <<(Print &obj, float arg) { obj.print(arg, 4); return obj; }
-
 #define NUM_SENSORS     3      // number of sensors in each array
 #define TIMEOUT         2500   // wait for 2500 microsecodns for sensor outputs to go low
-#define SIGNAL_PIN      A0
+#define NUM_SAMPLES     10    // how many samples over which to spread the running average.
+#define MIN_VALUE       200        //sets minimum threshold for brightness
+#define MAX_VALUE       2300       //sets max threshold for blackness
+#define ltLED           8
+#define rtLED           9
 
-int minValue = 500;  //sets minimum threshold for brightness
-int maxValue = 2000; //sets max threshold for blackness
+//create sensor array objects
+QTRSensorsRC ltSensor((unsigned char[]) {2, 3, 4}, NUM_SENSORS, TIMEOUT);
+QTRSensorsRC rtSensor((unsigned char[]) {5, 6, 7}, NUM_SENSORS, TIMEOUT);
 
 // Create arrays
-QTRSensorsRC ltFront((unsigned char[]) {2,  3,4}, NUM_SENSORS, TIMEOUT);
-QTRSensorsRC rtFront((unsigned char[]) {5, 6, 7}, NUM_SENSORS, TIMEOUT);
-//QTRSensorsRC ltRear((unsigned char[]) {8, 9, 10}, NUM_SENSORS, TIMEOUT);
-//QTRSensorsRC rtRear((unsigned char[]) {11, 12 ,13}, NUM_SENSORS, TIMEOUT);
+unsigned int ltValues[NUM_SENSORS];
+unsigned int rtValues[NUM_SENSORS];
 
-unsigned int sensorValues [NUM_SENSORS];
+/* History arrays to strore sensor values for running average. Array will          e.g:             Sensor #0   Sensor#1   Sensor#2
+ * have number of rows equal to the number of samples defined above, and                 Sample#0        2500       2500        211
+ * the number ofcolumns eqal to the number of sensors in each array                      Sample#1        2500       2500        186
+ * defined above.                                                                        Sample#2        2500       2500        234
+*/
+unsigned int ltValuesHist[NUM_SAMPLES][NUM_SENSORS];
+unsigned int rtValuesHist[NUM_SAMPLES][NUM_SENSORS];
+
+unsigned long ltValuesSum[NUM_SENSORS];
+unsigned long rtValuesSum[NUM_SENSORS];
+
+unsigned int ltValuesAvg[NUM_SENSORS];
+unsigned int rtValuesAvg[NUM_SENSORS];
+
+int sampleCount = 0;
 
 void setup() {
-  pinMode(SIGNAL_PIN, OUTPUT);
-  digitalWrite(SIGNAL_PIN, LOW);
   Serial.begin(115200);
+  
+  pinMode(ltLED, OUTPUT);
+  pinMode(rtLED, OUTPUT);
+  digitalWrite(ltLED, LOW);
+  digitalWrite(ltLED, LOW);
+
+  //reset arrays to middle of the road
+  for (int i = 0; i < NUM_SENSORS; i++) {
+    for (int j = 0; j < NUM_SAMPLES; j++) {
+      ltValuesHist[j][i] = TIMEOUT / 2;
+      rtValuesHist[j][i] = TIMEOUT / 2;
+    }
+
+    ltValuesSum[i] = (long) TIMEOUT * NUM_SAMPLES / 2;
+    rtValuesSum[i] = (long) TIMEOUT * NUM_SAMPLES / 2;
+  }
 }
 
 void loop() {
-  
-  ltFront.read(sensorValues);
-  contrastCheck();
-  Serial << sensorValues[0] << '\t' << sensorValues[1] << '\t' << sensorValues[3] << '\n';
-  
-  rtFront.read(sensorValues);
-  contrastCheck();
-  Serial << sensorValues[0] << '\t' << sensorValues[1] << '\t' << sensorValues[3] << '\n';
-  
-//  ltRear.read(sensorValues);
-//  contrastCheck();
-//  Serial << sensorValues[0] << '\t' << sensorValues[1] << '\t' << sensorValues[3] << '\n';
-//  
-//  ltFront.read(sensorValues);
-//  contrastCheck();
-//  Serial << sensorValues[0] << '\t' << sensorValues[1] << '\t' << sensorValues[3] << "\n\n\n";
-  
-}
+  //read sensor values
+  ltSensor.read(ltValues);
+  rtSensor.read(rtValues);
 
-void contrastCheck() {
-  boolean belowMin = false;
-  boolean aboveMax = false;
+  for (int i = 0; i < NUM_SENSORS; i++) {
+    //update sums
+    ltValuesSum[i] = ltValuesSum[i] - ltValuesHist[sampleCount][i] + ltValues[i];
+    rtValuesSum[i] = rtValuesSum[i] - rtValuesHist[sampleCount][i] + rtValues[i];
+    
+    //update history
+    ltValuesHist[sampleCount][i] = ltValues[i];
+    rtValuesHist[sampleCount][i] = rtValues[i];
+    
+    //calculate average
+    ltValuesAvg[i] = ltValuesSum[i] / NUM_SAMPLES;
+    rtValuesAvg[i] = rtValuesSum[i] / NUM_SAMPLES;
+  }
+  
+  boolean ltbelowMin = false;
+  boolean ltaboveMax = false;
+  boolean rtbelowMin = false;
+  boolean rtaboveMax = false;
+  static unsigned long ltDelay = 0;
+  static unsigned long rtDelay = 0;
+
+  //check average values against thresholds defined above
   for(unsigned char i = 0; i < NUM_SENSORS; i++) {
-    if(sensorValues[i] < minValue) {
-      belowMin = true;
-    }else if(sensorValues[i] > maxValue) {
-      aboveMax = true;
+    //check left sensor averages
+    if(ltValuesAvg[i] < MIN_VALUE) {
+      ltbelowMin = true;
+    }else if(ltValuesAvg[i] > MAX_VALUE) {
+      ltaboveMax = true;
+    }
+
+    //check right sensor averages
+    if(rtValuesAvg[i] < MIN_VALUE) {
+      rtbelowMin = true;
+    }else if(rtValuesAvg[i] > MAX_VALUE) {
+      rtaboveMax = true;
     }
   }
-  if(belowMin && aboveMax) {
-    digitalWrite(SIGNAL_PIN, HIGH);
-    delay(10);
-  }else digitalWrite(SIGNAL_PIN, LOW);
+
+  //control indicator pins
+  if(ltbelowMin && ltaboveMax) {
+    digitalWrite(ltLED, HIGH);
+    ltDelay = millis();
+  }else if (millis() - ltDelay > 100) digitalWrite(ltLED, LOW);
+  if(rtbelowMin && rtaboveMax) {
+    digitalWrite(rtLED, HIGH);
+    rtDelay = millis();
+  }else if (millis() - rtDelay > 100) digitalWrite(rtLED, LOW);
+
+
+  if (sampleCount < NUM_SAMPLES -1) {
+    sampleCount ++;
+  } else sampleCount = 0;
+
+//    //debugging
+//  Serial.print("Sample Count: ");
+//  Serial.println(sampleCount);
+//  Serial.println("Sensor Values");
+//  for (int i = 0; i < NUM_SENSORS; i++) {
+//    Serial.print(ltValues[i]);
+//    Serial.print('\t');
+//  }
+//  for (int i = 0; i < NUM_SENSORS; i++) {
+//    Serial.print(rtValues[i]);
+//    Serial.print('\t');
+//  }
+//  
+//  Serial.println("\nSensor History");
+//  for (int j = 0; j < NUM_SAMPLES; j++) {
+//    for (int i = 0; i < NUM_SENSORS; i++) {
+//      Serial.print(ltValuesHist[j][i]);
+//      Serial.print('\t');
+//    }
+//    for (int i = 0; i < NUM_SENSORS; i++) {
+//      Serial.print(rtValuesHist[j][i]);
+//      Serial.print('\t');
+//    }
+//    Serial.println();
+//  }
+//
+//  Serial.println("Sensor Sums");
+//  for (int i = 0; i < NUM_SENSORS; i++) {
+//    Serial.print(ltValuesSum[i]);
+//    Serial.print('\t');
+//  }
+//  for (int i = 0; i < NUM_SENSORS; i++) {
+//    Serial.print(rtValuesSum[i]);
+//    Serial.print('\t');
+//  }
+//  Serial.println();
+//  
+//  Serial.println("Sensor Averages");
+//  for (int i = 0; i < NUM_SENSORS; i++) {
+//    Serial.print(ltValuesAvg[i]);
+//    Serial.print('\t');
+//  }
+//  for (int i = 0; i < NUM_SENSORS; i++) {
+//    Serial.print(rtValuesAvg[i]);
+//    Serial.print('\t');
+//  }
+//  Serial.println('\n');
+//  delay(1000);
 }
